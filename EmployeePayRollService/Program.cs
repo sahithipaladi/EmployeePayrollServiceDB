9bb25c5209b22fa3c07a41653b8ae95a5258dda0
Sahithi Paladi <sahithi.p@tvsnext.io>
2021-08-05 18:31:41 +0530

    Connecting to database

---
+++ EmployeePayRollService/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Payroll Service uing ADO.Net");
+        }
+    }
+}
