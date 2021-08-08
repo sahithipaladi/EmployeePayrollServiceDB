ab397592a162b719b242f854ded17de47e08147a
Sahithi Paladi <sahithi.p@tvsnext.io>
2021-08-08 19:15:34 +0530

    [sahithi] Removing Employee

---
+++ EmployeePayRollService/dbo.RemoveEmployee.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE dbo.RemoveEmployee
+(
+@Id int
+)
+AS
+BEGIN
+	delete from employee where EmployeeId=@Id
+	delete from employee_payroll where id=@Id
+	delete from payroll_details where Id=@Id
+END
